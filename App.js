2867d0754f2f41698ec18c61d5974e25bb9e3b8e
NYCKOLY KETTLYN SIQUEIRA DA SILVA <nyckoly.kssilva@senacsp.edu.br>
2024-12-06 14:30:28 -0300

    atividade finalizada.

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import Cadastro from './Cadastro';
+
+const App = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Cadastro />
+        </SafeAreaView>
+    );
+};
+
+export default App;
